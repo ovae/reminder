e78759dba7a6cb27b45c15e0f7757f8a165d7dd4
ovae <loersikh.harek@gmail.com>
2014-11-06 11:46:46 +0100

    smale fix.

---
+++ src/rem/RemJSON.java
@@ -0,0 +1,79 @@
+package bin.rem;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+
+public class RemJSON {
+	
+	//private JSONObject obj = new JSONObject();
+	private List taskList = new LinkedList();
+	JSONObject Mainobj = new JSONObject();
+
+	
+	@SuppressWarnings("unchecked")
+	public void setTask(String[] tableHead, String topic, String about, String begin, String end, String status){
+		JSONObject obj = new JSONObject();
+		obj.put(""+tableHead[0]+"", topic);
+		obj.put("About", about);
+		obj.put("Begin", begin);
+		obj.put("End", end);
+		obj.put("Status", status);
+		taskList.add(obj);
+		Mainobj.put("Tasks", taskList);
+	}
+	
+	/**
+	 * 
+	 */
+	public void writeJsonToFile(File path){
+			FileWriter file;
+			try {
+				file = new FileWriter(path);
+				try {
+					file.write( JSONValue.toJSONString(Mainobj)+"\n");
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					file.flush();
+					file.close();
+				}
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+	}
+	
+	
+	/**
+	 * 
+	 */
+	private void updateJsonFile(){
+		
+	}
+	
+	/**
+	 * 
+	 */
+	@SuppressWarnings("null")
+	public Object loadJsonFile(File tempFile){
+		
+		String[][] nullA = null;
+
+		return nullA;
+	}
+	
+	/**
+	 * 
+	 * @param file
+	 */
+	private void removeJsonFile(File file){
+		
+	}
+}
